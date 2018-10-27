1befadf3732767c0faa58f0a5af005f30035a017
wieslawo <wieslawkatondirect@gmail.com>
2018-10-27 23:36:18 +0200

    Added DragMove on MouseDown

--- WpfAppShortCut/App.xaml.cs
+++ WpfAppShortCut/App.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfAppShortCut
@@ -13,5 +8,23 @@ namespace WpfAppShortCut
     /// </summary>
     public partial class App : Application
     {
+        //protected override void OnActivated(EventArgs e)
+        //{
+        //    base.OnActivated(e);
+        //    if (MainWindow != null)
+        //    {
+        //        MainWindow.Height = 100;
+        //        MainWindow.Width = 400;
+        //        //var barCommands = (System.Windows.Controls.Grid)MainWindow.Template.FindName("PART_CommandsBar", MainWindow);
+        //        //barCommands.Height = 0;
+        //        //var barTopApp = (WpfLightToolkit.Controls.LightAppBar)MainWindow.Template.FindName("PART_TopAppBar", MainWindow);
+        //        //barTopApp.Height = 0;
+        //        //var border = (System.Windows.Controls.Border)MainWindow.Template.FindName("BorderWindow", MainWindow);
+        //        ////border.BorderThickness = new Thickness(0);
+        //        //border.BorderBrush = new SolidColorBrush(Color.FromRgb(126, 125, 126));
+        //    }
+        //}
+
+
     }
 }
--- WpfAppShortCut/MainWindow.xaml.cs
+++ WpfAppShortCut/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfAppShortCut
 {
@@ -23,6 +12,17 @@ namespace WpfAppShortCut
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void MainWindow_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
